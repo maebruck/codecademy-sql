7eb8eb73979164a52a6c40916b3cc8b947ee7373
tbruckbauer <1592639+tbruckbauer@users.noreply.github.com>
2020-04-10 18:37:00 +0200

    add to project 1

--- P1_Watching the stock market/stocks.sql
+++ P1_Watching the stock market/stocks.sql
@@ -6,17 +6,55 @@ name TEXT,
 datetime DATETIME,
 price DECIMAL(20,2)
 );
-
+-- used random data.
 INSERT INTO stocks
-VALUES ("AAPL", "Apple Inc.", "2020-01-01 18:00:00", 300.35),
-("AAPL", "Apple Inc.", "2020-01-01 18:00:00", 300.35);
+VALUES ("AAPL", "Apple Inc.", "2019-05-01 18:00:00", 210.52),
+("AAPL", "Apple Inc.", "2019-06-03 18:00:00", 173.30),
+("AAPL", "Apple Inc.", "2019-07-01 18:00:00", 184.23),
+("AAPL", "Apple Inc.", "2019-08-01 18:00:00", 254.23),
+("AAPL", "Apple Inc.", "2019-09-01 18:00:00", 270.32),
+("AAPL", "Apple Inc.", "2019-10-01 18:00:00", 285.12),
+("AAPL", "Apple Inc.", "2019-11-01 18:00:00", 295.93),
+("AAPL", "Apple Inc.", "2019-12-01 18:00:00", 318.92),
+("AAPL", "Apple Inc.", "2020-01-01 18:00:00", 300.35),
+("AAPL", "Apple Inc.", "2020-02-01 18:00:00", 354.22),
+("AAPL", "Apple Inc.", "2020-03-01 18:00:00", 302.23),
+("AAPL", "Apple Inc.", "2020-04-01 18:00:00", 270.35),
+("GOOG", "Alphabet Inc.", "2019-05-01 18:00:00", 1210.52),
+("GOOG", "Alphabet Inc.", "2019-06-03 18:00:00", 1173.30),
+("GOOG", "Alphabet Inc.", "2019-07-01 18:00:00", 1184.23),
+("GOOG", "Alphabet Inc.", "2019-08-01 18:00:00", 1154.23),
+("GOOG", "Alphabet Inc.", "2019-09-01 18:00:00", 1270.32),
+("GOOG", "Alphabet Inc.", "2019-10-01 18:00:00", 1195.12),
+("GOOG", "Alphabet Inc.", "2019-11-01 18:00:00", 1232.93),
+("GOOG", "Alphabet Inc.", "2019-12-01 18:00:00", 1212.92),
+("GOOG", "Alphabet Inc.", "2020-01-01 18:00:00", 1300.35),
+("GOOG", "Alphabet Inc.", "2020-02-01 18:00:00", 1354.22),
+("GOOG", "Alphabet Inc.", "2020-03-01 18:00:00", 1102.23),
+("GOOG", "Alphabet Inc.", "2020-04-01 18:00:00", 1270.35),
+("MSFT", "Microsoft Corp.", "2019-05-01 18:00:00", 110.52),
+("MSFT", "Microsoft Corp.", "2019-06-03 18:00:00", 173.30),
+("MSFT", "Microsoft Corp.", "2019-07-01 18:00:00", 184.23),
+("MSFT", "Microsoft Corp.", "2019-08-01 18:00:00", 154.23),
+("MSFT", "Microsoft Corp.", "2019-09-01 18:00:00", 170.32),
+("MSFT", "Microsoft Corp.", "2019-10-01 18:00:00", 195.12),
+("MSFT", "Microsoft Corp.", "2019-11-01 18:00:00", 132.93),
+("MSFT", "Microsoft Corp.", "2019-12-01 18:00:00", 112.92),
+("MSFT", "Microsoft Corp.", "2020-01-01 18:00:00", 100.35),
+("MSFT", "Microsoft Corp.", "2020-02-01 18:00:00", 154.22),
+("MSFT", "Microsoft Corp.", "2020-03-01 18:00:00", 102.23),
+("MSFT", "Microsoft Corp.", "2020-04-01 18:00:00", 170.35);
 
--- o	Hint: See here 156 for a review of the CREATE TABLE syntax. What data type 116 should each field be?
--- •	The stocks table should have a column for symbol, name, datetime, and price.
--- •	Collect your data! Choose 3 times throughout the day to document the price of each stock and continue for at least 1 week. You can do this moving forward, or just take a retroactive look at the stock market by taking data historically from regular intervals (e.g. the first of the month for the last six months).
--- o	Hint: See here 80 for a review of the INSERT INTO syntax. When inserting the datetime, use the standard format ‘yyyy-mm-dd hh:mm:ss’. Use the strftime() 96 function to help you get the datetime of ‘now’.
 -- Queries: Perform basic analysis on the data and identify trends.
 -- •	What are the distinct stocks in the table?
+SELECT DISTINCT symbol FROM stocks;
 -- •	Query all data for a single stock. Do you notice any overall trends?
+SELECT * FROM stocks WHERE symbol = "AAPL";
+-- firstly increase, drop after february
 -- •	Which rows have a price above 100? between 40 to 50, etc?
+SELECT * FROM stocks WHERE price > 200;
+SELECT COUNT(*) FROM stocks WHERE price > 200;
 -- •	Sort the table by price. What are the minimum and maximum prices?
+SELECT *, MIN(price) AS min FROM stocks;
+SELECT *, MAX(price) AS max FROM stocks;
+SELECT * FROM stocks ORDER BY price;
